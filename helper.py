26153d506d8d75785a62aaa363ae287843b23cb5
nikhilsaini098 <nikhilsaini01012001@gmail.com>
2025-03-14 15:15:46 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -86,7 +86,7 @@ def time_name():
 
 async def download_video(url, cmd, name):
     time.sleep(2)
-    download_cmd = f'{cmd} -R infinite --fragment-retries 25 --socket-timeout 50 --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32" --user-agent "Mozilla/5.0 (Windows NT 10.0; Wi[...]
+    download_cmd = f'{cmd} -R infinite --fragment-retries 25 --socket-timeout 50 --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32" --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"'
     global failed_counter
     print(download_cmd)
     logging.info(download_cmd)
