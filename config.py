8479a03a89f901f14b175ceaec361e29948cd26f
nikhilsaini098 <nikhilsaini01012001@gmail.com>
2025-03-14 13:56:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 API_ID    = os.environ.get("API_ID", "")
 API_HASH  = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-OWNER = int(os.environ.get("OWNER", ""))
+OWNER = int(os.environ.get("OWNER", "5680454765"))
 
 LOG = True  # Set to False if logging is not required
 
